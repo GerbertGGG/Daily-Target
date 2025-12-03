e1b211a7f17af3d31e74e42a9f8f90c9d5a6e35f
GerbertGGG <hausibauster@aol.com>
2025-12-03 10:45:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const BASE_URL = "https://intervals.icu/api/v1";
 
-
 const INTERVALS_API_KEY = "1xg1v04ym957jsqva8720oo01";
 const INTERVALS_ATHLETE_ID = "i105857";
 const INTERVALS_TARGET_FIELD = "TageszielTSS";
