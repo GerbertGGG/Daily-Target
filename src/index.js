66efda04284e10601498a9353805fe65522c8b9f
GerbertGGG <hausibauster@aol.com>
2025-12-08 11:09:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const BASE_URL = "https://intervals.icu/api/v1";
-
 // 🔥 Hardcoded Variablen – HIER deine Werte eintragen!
 const INTERVALS_API_KEY = "1xg1v04ym957jsqva8720oo01";
 const INTERVALS_ATHLETE_ID = "i105857";
